44e9688e1e21a8665b233073bf7c0610aa6c5ca8
Jan Nosek <100805713+S1ngleWasTaken@users.noreply.github.com>
2023-09-22 15:14:58 +0200

    form validation and creating books as objects

--- script.js
+++ script.js
@@ -3,36 +3,54 @@ const titleInput = document.querySelector('#title-input');
 const authorInput = document.querySelector('#author-input');
 const readInput = document.querySelector('#read-input');
 const form = document.querySelector('form')
+const submitBtn = document.querySelector('#submit-btn')
+const warning = document.querySelector('.warning')
 
 
 let formDisplayed = false;
 let library = [];
 
 
-let Book = function(title, author, rating){
+let Book = function(title, author, read){
     this.title = title;
     this.author = author;
-    this.rating = rating;
+    this.read = read;
 }
 let displayForm = function(){
     if(formDisplayed === false){
         form.style.display = 'grid'
         formDisplayed = !formDisplayed
-        console.log(formDisplayed);
     }else{
         form.style.display = 'none'
         formDisplayed = !formDisplayed
     }
 }
+
+let isValid = function(){
+    if(titleInput.value !== '' && authorInput.value !== ''){
+        addBook();
+        warning.style.display = 'none'
+    }
+    else{
+        warning.style.display = 'block'
+    }
+}
+
+let addBook = function(){
+    displayForm()
+    library.push(new Book(titleInput.value, authorInput.value, readInput.checked))
+    console.log(library);
+}
+
 addBookBtn.addEventListener('click', displayForm)
-console.log(library);
+submitBtn.addEventListener('click', isValid)
 
 
 
 /* 
 TITLE AUTHOR READ konstanty check
-vypnout zapnout formular
-vytvořit objekt knihy
+vypnout zapnout formular check
+vytvořit objekt knihy check
 vytvořit div knihy s jejim nazvem autorem a jestli to četl checkboxem
 
 
